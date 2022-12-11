e8dc5f64e0228e4c0afe8ed17eb488b8aa8f564e
JIBON <jibon2230@gmail.com>
2022-12-11 09:11:04 +0600

    row bg colorchange  added

--- src/App.js
+++ src/App.js
@@ -1,54 +1,101 @@
 import logo from './logo.svg';
 import './App.css';
-import { useEffect, useRef, useState } from 'react';
-
+import { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { toast, Toaster } from 'react-hot-toast';
 function App() {
   const [isAsc, setIsAsc] = useState(true)
 
-  const [status, setStatus] = useState('')
-  const [tabledata, settableData] = useState([])
 
-  useEffect(() => {
 
-    fetch(`http://localhost:5000/task?order=${isAsc ? 'asc' : 'dsc'}`)
+
+
+
+  const { data: tabledata = [], isLoading, refetch } = useQuery({
+
+
+    queryKey: [isAsc],
+    queryFn: () => fetch(`http://localhost:5000/task?order=${isAsc ? 'asc' : 'dsc'}`)
       .then(res => res.json())
-      .then(data => settableData(data))
+
+
+
+  })
 
 
 
 
-  }, [isAsc])
 
 
 
-
-  const selected = (id) => {
+  const selected = id => {
 
     console.log(id)
+    fetch(`http://localhost:5000/status/${id}`, {
 
-    setStatus(true)
 
-    fetch(`http://localhost:5000/status?id=${id}`)
+      method: "PUT",
+
+
+
+
+    })
       .then(res => res.json())
-      .then(data => console.log(data))
+      .then(data => {
+
+        console.log(data)
+        if (data.modifiedCount > 0) {
+
+          toast.success('Status False')
+          refetch()
+
+        }
+
+
+
+      })
+
 
 
 
 
   }
-  const notselected = (id) => {
 
 
-    setStatus(false)
-    console.log(id, 'not')
-    fetch(`http://localhost:5000/status?id=${id}`)
+  const notselected = id => {
+
+    fetch(`http://localhost:5000/!status/${id}`, {
+
+
+      method: "PUT",
+
+
+
+
+    })
+
       .then(res => res.json())
-      .then(data => console.log(data))
+      .then(data => {
 
+        console.log(data)
+        if (data.modifiedCount > 0) {
+
+          toast.success('Status true')
+          refetch()
+
+        }
+
+
+
+      })
 
 
 
   }
+
+
+
+
 
   console.log(tabledata)
 
@@ -60,7 +107,7 @@ function App() {
 
 
           <thead>
-            <tr className=''>
+            <tr >
               <th></th>
               <th>First Name</th>
               <th>Last name</th>
@@ -74,21 +121,25 @@ function App() {
           </thead>
           {
 
-            tabledata?.map((table, index) => <tbody key={index}>
-              <tr>
+            tabledata?.map((table, index) => <tbody  key={index}>
+              <tr className={table.status ? 'active' : 'bg-green-600'} >
                 <th>{index + 1}</th>
                 <td>{table.firstName}</td>
                 <td>{table.lastName} </td>
                 <td>{table.age}</td>
                 <td>{table.fullname}</td>
-                <td>{!status ? <button onClick={() => selected(table._id)}>True</button> : <button onClick={() => notselected(table._id)}>False</button>}</td>
+                <td>{table.status ? <button className='text-green-600' onClick={() => selected(table._id)}>True</button> : <button className='text-red-600' onClick={() => notselected(table._id)}>Failed</button>}</td>
               </tr>
 
             </tbody>)
 
           }
         </table>
+
+        
       </div>
+      
+      <Toaster></Toaster>
     </div>
   );
 }
