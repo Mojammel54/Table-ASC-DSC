892b988fed2dc07b3b488b3af0a088b472ab7e36
JIBON <jibon2230@gmail.com>
2022-12-11 07:49:54 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,94 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useRef, useState } from 'react';
 
 function App() {
+  const [isAsc, setIsAsc] = useState(true)
+
+  const [status, setStatus] = useState('')
+  const [tabledata, settableData] = useState([])
+
+  useEffect(() => {
+
+    fetch(`http://localhost:5000/task?order=${isAsc ? 'asc' : 'dsc'}`)
+      .then(res => res.json())
+      .then(data => settableData(data))
+
+
+
+
+  }, [isAsc])
+
+
+
+
+  const selected = (id) => {
+
+    console.log(id)
+
+    setStatus(true)
+
+    fetch(`http://localhost:5000/status?id=${id}`)
+      .then(res => res.json())
+      .then(data => console.log(data))
+
+
+
+
+  }
+  const notselected = (id) => {
+
+
+    setStatus(false)
+    console.log(id, 'not')
+    fetch(`http://localhost:5000/status?id=${id}`)
+      .then(res => res.json())
+      .then(data => console.log(data))
+
+
+
+
+  }
+
+  console.log(tabledata)
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="overflow-x-auto">
+        <table className="lg:table w-full table-auto">
+
+
+          <thead>
+            <tr className=''>
+              <th></th>
+              <th>First Name</th>
+              <th>Last name</th>
+              <th>Age</th>
+              <th>Fullname</th>
+              <th>Status</th>
+              <th>Action</th>
+              <th> <button className='btn btn-secondary' onClick={() => setIsAsc(!isAsc)}>{isAsc ? 'dsc' : 'asc'}</button></th>
+
+            </tr>
+          </thead>
+          {
+
+            tabledata?.map((table, index) => <tbody key={index}>
+              <tr>
+                <th>{index + 1}</th>
+                <td>{table.firstName}</td>
+                <td>{table.lastName} </td>
+                <td>{table.age}</td>
+                <td>{table.fullname}</td>
+                <td>{!status ? <button onClick={() => selected(table._id)}>True</button> : <button onClick={() => notselected(table._id)}>False</button>}</td>
+              </tr>
+
+            </tbody>)
+
+          }
+        </table>
+      </div>
     </div>
   );
 }
